28a6d8f9054f3a159643e690383bb8f85ee87a1f
pabloosabaterr <pabloosabaterr@gmail.com>
2026-02-05 11:23:14 +0100

    refactor: documentacion y enum
    ahora se usa un enum para ecapsular los tokens del lexer

--- lexico1.h
+++ lexico1.h
@@ -1,17 +1,24 @@
-#define             BEGINN          1
-#define             END             2
-#define             READ            3
-#define             WRITE           4
-#define             ID              5
-#define             INTLITERAL      6
-#define             LPAREN          7
-#define             RPAREN          8
-#define             SEMICOLON       9
-#define             COMMA           10
-#define             ASSIGNOP        11
-#define             PLUSOP          12
-#define             MINUSOP         13
-#define             MAIN            15
-#define             PRINT           16
-#define             RBRACKET        17
-#define             LBRACKET        18
+#ifndef LEXICO1_H
+#define LEXICO1_H
+
+typedef enum {
+    TOK_BEGINN = 1,
+    TOK_END,
+    TOK_READ,
+    TOK_WRITE,
+    TOK_ID,
+    TOK_INTLITERAL,
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_SEMICOLON,
+    TOK_COMMA,
+    TOK_ASSIGNOP,
+    TOK_PLUSOP,
+    TOK_MINUSOP,
+    TOK_MAIN,
+    TOK_PRINT,
+    TOK_RBRACKET,
+    TOK_LBRACKET
+} Token;
+
+#endif /* LEXICO1_H */ 
