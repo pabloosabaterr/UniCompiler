9744cfa53ef7e5811fe94937888a404d08e80e46
Pablo <pabloosabaterr@gmail.com>
2026-02-05 12:38:47 +0100

    feat: añadidos tokens ampliacion

--- lexer.h
+++ lexer.h
@@ -2,23 +2,50 @@
 #define LEXICO1_H
 
 typedef enum {
-    TOK_BEGINN = 1,
+    // Palabras reservadas
+    TOK_BEGIN = 1,
     TOK_END,
     TOK_READ,
-    TOK_WRITE,
-    TOK_ID,
-    TOK_INTLITERAL,
-    TOK_LPAREN,
-    TOK_RPAREN,
-    TOK_SEMICOLON,
-    TOK_COMMA,
-    TOK_ASSIGNOP,
-    TOK_PLUSOP,
-    TOK_MINUSOP,
     TOK_MAIN,
     TOK_PRINT,
+    TOK_IF,
+    TOK_ELSE,
+    TOK_WHILE,
+    TOK_FOR,
+    TOK_DO,
+    TOK_BREAK,
+    // Identificadores y literales
+    TOK_ID,
+    TOK_INT,
+    TOK_INTLITERAL,
+    // Delimitadores
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_LBRACKET,
     TOK_RBRACKET,
-    TOK_LBRACKET
+    TOK_SEMICOLON,
+    TOK_COMMA,
+    // Operadores de asignación
+    TOK_ASSIGNOP,
+    TOK_PLUSEQ,
+    TOK_MINUSEQ,
+    TOK_MULTEQ,
+    TOK_DIVEQ,
+    TOK_MODEQ,
+    // Operadores aritméticos
+    TOK_PLUSOP,
+    TOK_MINUSOP,
+    TOK_MULTOP,
+    TOK_DIVOP,
+    TOK_MODOP,
+    // Operadores logicos y de comparación
+    TOK_EQ,
+    TOK_NEQ,
+    TOK_LT,
+    TOK_GT,
+    TOK_LTEQ,
+    TOK_GTEQ,
+    TOK_NOT,
 } Token;
 
 #endif /* LEXICO1_H */ 
