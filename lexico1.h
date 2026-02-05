8af8140710db0f350dbc106e7c0ef6d7dac53050
pabloosabaterr <pabloosabaterr@gmail.com>
2026-02-05 11:02:01 +0100

    feat: comentarios multiLinea

---
+++ lexico1.h
@@ -0,0 +1,17 @@
+#define             BEGINN          1
+#define             END             2
+#define             READ            3
+#define             WRITE           4
+#define             ID              5
+#define             INTLITERAL      6
+#define             LPAREN          7
+#define             RPAREN          8
+#define             SEMICOLON       9
+#define             COMMA           10
+#define             ASSIGNOP        11
+#define             PLUSOP          12
+#define             MINUSOP         13
+#define             MAIN            15
+#define             PRINT           16
+#define             RBRACKET        17
+#define             LBRACKET        18
